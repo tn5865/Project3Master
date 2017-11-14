95a5ace0bde0317b5f0410cd9f782042fe69bef9
Jacob <jkrupa46@gmail.com>
2017-11-14 03:01:14 -0600

    fixing some spacing for javascript and adding new email and password

--- js/script.js
+++ js/script.js
@@ -1,38 +1,37 @@
 function validateForm() {
-    'use strict';
+  'use strict';
 
-    // Get references to the form elements:
-    var email = document.getElementById('email');
-    var password = document.getElementById('password');
+  // Get references to the form elements:
+  var email = document.getElementById('email');
+  var password = document.getElementById('password');
 
-    // Validate the login
-    if ((email.value.length > 0) && (password.value.length > 0)) {
-        return true;
-    } else {
-        alert('Please complete the form!');
-        return false;
-    }
+  // Validate the login
+  if ((email.value.length > 0) && (password.value.length > 0)) {
+    return true;
+  } 
+  else {
+    alert('Please complete the form!');
+    return false;
+  }
 }
 
 function check(form) {
-
-    var emailArray = ("myemail@live.com", "");
-    var passwordArray = ("MyLogIn", "");
-
-    if (email.value == "email" && password.value == "password") {
-        window.open('myaccount.html');
-    } else {
-        alert('Please enter correct username or password!');
-        return false;
-    }   
+  var emailArray = ("email@gmail.com", "");
+  var passwordArray = ("password", "");
+  
+  if (email.value == "email" && password.value == "password") {
+    window.open('myaccount.html');
+  } 
+  else {
+    alert('Please enter correct username or password!');
+    return false;
+  }   
 }
 
 function init() {
-    'use strict';
-
-    // Confirm that document.getElementById() can be used:
-    if (document && document.getElementById) {
-        var loginForm = document.getElementById('lgform');
-        loginForm.onsubmit = validateForm;
-    }
+  'use strict';
+  if (document && document.getElementById) {
+    var loginForm = document.getElementById('lgform');
+    loginForm.onsubmit = validateForm;
+  }
 }
